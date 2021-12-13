1ab0096eefe505d5984589d696d9017e730c755c
M4tasG <grybauskas.matas@gmail.com>
2021-12-13 18:18:11 +0200

    added ProfileDetailView to urls

--- simpleblog/blog/urls.py
+++ simpleblog/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import MainView, PostDetailView, CreatePostView
+from .views import MainView, PostDetailView, CreatePostView, ProfileDetailView
 
 urlpatterns = [
     path('', MainView.as_view(), name='blog-main'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='blog-post'),
     path('post/new/', CreatePostView.as_view(), name='blog-post-new'),
+    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='blog-profile'),
+
 ]
